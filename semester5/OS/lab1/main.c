f4a51fb155471e2cb79a4b71f2da8d8cea4faf9b
Oleksii <o_oleksyshyn@ukr.net>
2020-10-07 01:08:52 +0300

    added cond vars

--- semester5/OS/lab1/main.c
+++ semester5/OS/lab1/main.c
@@ -6,10 +6,14 @@
 bool f_res, g_res;
 bool f_ready = false, g_ready = false;
 
+pthread_mutex_t manager_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t something_happened = PTHREAD_COND_INITIALIZER;
+
 void* calc_f(void* args) {
     int* x = (int *) args;
     f_res = f_func_or(*x);
     f_ready = true;
+    pthread_cond_signal(&something_happened);
     return NULL;
 }
 
@@ -17,40 +21,52 @@ void* calc_g(void* args) {
     int* x = (int *) args;
     g_res = g_func_or(*x);
     g_ready = true;
+    pthread_cond_signal(&something_happened);
     return NULL;
 }
 
 bool need_quit = false;
-bool is_running = false;
+
+pthread_cond_t wait_for_quit = PTHREAD_COND_INITIALIZER;
 
 void* check_quit_message(void* arg) {
-    is_running = true;
     char answer;
+    printf("Do you you want to stop calculation?\n(y) yes\n(n) no\n");
     scanf("%s", &answer);
     if (answer == 'y') {
         need_quit = true;
-        is_running = false;
+        pthread_cond_signal(&something_happened);
         return NULL;
     }
     need_quit = false;
-    is_running = false;
+    pthread_cond_signal(&wait_for_quit);
+    return NULL;
+}
+
+void* wait_for(void* arg) {
+    sleep(1000);
+    pthread_cond_signal(&wait_for_quit);
     return NULL;
 }
 
 bool show_quit_message() {
-    printf("Are you sure you want to stop calculation?\n(y) yes\n(n) no\n");
     need_quit = false;
+
+    pthread_mutex_t wait_mutex = PTHREAD_MUTEX_INITIALIZER;
+    pthread_mutex_lock(&wait_mutex);
 
     pthread_t thread_check_quit;
     pthread_create(&thread_check_quit, NULL, check_quit_message, NULL);
+    pthread_t thread_wait_quit;
+    pthread_create(&thread_wait_quit, NULL, wait_for, NULL);
 
-    for (int i = 0; i < 15; ++i) {
-        // wait 15 second for answer
-        sleep(1);
-        if (!is_running)
-            break;
-    }
+    pthread_cond_wait(&wait_for_quit, &wait_mutex);
+
+    pthread_mutex_unlock(&wait_mutex);
+
     pthread_cancel(thread_check_quit);
+    pthread_cancel(thread_wait_quit);
+
 
     if (need_quit) {
         printf("Cancelling the calculations...\n");
@@ -61,24 +77,54 @@ bool show_quit_message() {
     }
 }
 
-void* parse_input(void* args) {
-    char c;
+pthread_mutex_t quit_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t quit_cond = PTHREAD_COND_INITIALIZER;
+
+pthread_t* thread_read = NULL;
+
+void* wait(void * arg) {
+    sleep(15);
+    pthread_cond_signal(&quit_cond);
+    return NULL;
+}
+
+void* parse_q(void * arg) {
+    char answer;
     while (true) {
-        scanf("%c", &c);
-        if (c == 'q') {
-            bool quit = show_quit_message();
-            if (quit)
-                return NULL;
+        scanf("%s", &answer);
+        if (answer == 'q') {
+            pthread_cond_signal(&quit_cond);
+            return NULL;
         }
     }
 }
 
+void* parse_input(void* args) {
+    char c;
+    while (true) {
+        pthread_t thread_wait;
+        pthread_t thread_parse_q;
+
+        pthread_create(&thread_wait, NULL, wait, NULL);
+        pthread_create(&thread_parse_q, NULL, parse_q, NULL);
+
+        pthread_mutex_lock(&quit_mutex);
+        pthread_cond_wait(&quit_cond, &quit_mutex);
+        pthread_cancel(thread_wait);
+        pthread_cancel(thread_parse_q);
+        bool quit = show_quit_message();
+        pthread_mutex_unlock(&quit_mutex);
+    }
+}
+
 void manager() {
-    printf("Doing calculations....\n");
+    printf("Doing calculation of g(x) or f(x)....\n");
     bool f_ready_written = false;
     bool g_ready_written = false;
     while (true) {
-        sleep(3);
+        pthread_mutex_lock(&manager_mutex);
+        pthread_cond_wait(&something_happened, &manager_mutex);
+        pthread_mutex_unlock(&manager_mutex);
         if (need_quit)
             return;
         if (g_ready && f_ready) {
